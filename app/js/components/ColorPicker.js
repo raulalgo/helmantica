66fb93c589245c04ced3f9432a8acfb373a444bb
Raúl Álvarez González <raulalgo@gmail.com>
2016-01-11 10:44:48 +0000

    Everything is conected

--- app/js/components/ColorPicker.js
+++ app/js/components/ColorPicker.js
@@ -32,13 +32,13 @@ class ColorPicker extends React.Component{
           R:<br /><input type="range" name="red" min="0" max="255" step="1" defaultValue={this.props.r} onChange={this.handleChange} /> {this.props.r}<br />
           G:<br /><input type="range" name="green" min="0" max="255" step="1" defaultValue={this.props.g} onChange={this.handleChange} /> {this.props.g}<br />
           B:<br /><input type="range" name="blue" min="0" max="255" step="1" defaultValue={this.props.b} onChange={this.handleChange} /> {this.props.b}
-          <Tuner nombre="tuner" valor="100" onUserInput={this.handleChange} />
+          
         </div>
     );
   }
 
-  handleChange(owner,name,value) {
-    this.props.onUserInput(owner,name,value);
+  handleChange(event) {
+    this.props.onUserInput(this.props.name,event.target.name,event.target.value);
   }
 }
 
