f8b06d2d3b3cb3c954d780a0fcb77c36343a66db
Raúl Álvarez González <raulalgo@gmail.com>
2016-01-18 16:14:36 +0000

    build working after updating

--- app/js/components/ColorPicker.js
+++ app/js/components/ColorPicker.js
@@ -31,14 +31,17 @@ class ColorPicker extends React.Component{
           <ColorDisplay ref="displayer" bgRed={this.props.r} bgGreen={this.props.g} bgBlue={this.props.b} cdClass="mainColor centeredBox" />
           R:<br /><input type="range" name="red" min="0" max="255" step="1" defaultValue={this.props.r} onChange={this.handleChange} /> {this.props.r}<br />
           G:<br /><input type="range" name="green" min="0" max="255" step="1" defaultValue={this.props.g} onChange={this.handleChange} /> {this.props.g}<br />
-          B:<br /><input type="range" name="blue" min="0" max="255" step="1" defaultValue={this.props.b} onChange={this.handleChange} /> {this.props.b}
-          
+          B:<br /><input type="range" name="blue" min="0" max="255" step="1" defaultValue={this.props.b} onChange={this.handleChange} /> {this.props.b}<br />
         </div>
     );
   }
 
   handleChange(event) {
     this.props.onUserInput(this.props.name,event.target.name,event.target.value);
+  }
+
+  tunerChange(chanel,value) {
+    this.props.onUserInput(this.props.name,chanel,value)
   }
 }
 
--- app/js/components/Tuner.js
+++ app/js/components/Tuner.js
@@ -4,24 +4,38 @@ import React from 'react';
 
 
 class ColorDisplay extends React.Component{
+	valueField
+	valueRange
 
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentWillMount() {
+  	this.valueField = this.props.defaultValue
+  	this.valueRange = this.props.defaultValue
   }
 
   render() {
     return (
     	<div>
-			<input type="range" name={this.props.nombre} min="0" max="255" defaultValue={this.props.valor} step="1" onChange={this.handleChange} />
-			<input type="field" defaultValue={this.props.valor} />
+			<input type="range" name={this.props.name} min="0" max="255" defaultValue={this.valueRange} step="1" onChange={this.handleChangeRange} />
+			<input type="field" defaultValue={this.valueField} onChange={this.handleChangeField} />
 		</div>
     );
   }
 
   handleChange(event) {
-    this.props.onUserInput(this.props.nombre,event.target.name,event.target.value);
-    alert(this.props.nombre)
+    //alert("hey")
+    //alert("name: " + this.props.name + "; value: " + event.target.value)
   }
+
+  handleChangeRange(event) {
+  	//alert("hey")
+  	this.valueField = event.target.value
+  }
+
 }
 
 export default ColorDisplay;
