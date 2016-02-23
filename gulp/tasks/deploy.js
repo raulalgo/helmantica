cea1fa15d4be18b53b1d4a7ce6c7289d02e17def
Raúl Álvarez González <raulalgo@gmail.com>
2016-02-23 15:58:29 +0000

    trying to deploy to heroku

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,10 +1,15 @@
 'use strict';
 
 import gulp from 'gulp';
+import runSequence from 'run-sequence'
 //import config from '../config';
 
-gulp.task('deploy', ['prod'], function() {
+gulp.task('deploy', ['prod'], function(cb) {
 
   // Deploy to hosting environment
+  cb = cb || function() {};
 
+  global.isProd = true;
+
+  runSequence(['sass', 'imagemin', 'browserify', 'copyFonts', 'copyIndex', 'copyIcons', 'watch'], cb);
 });
