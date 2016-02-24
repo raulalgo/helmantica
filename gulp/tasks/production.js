edbc65da496055e73f3f0b1eb05260afbed839ad
Raúl Álvarez González <raulalgo@gmail.com>
2016-02-24 12:03:12 +0000

    Production task now serves files. Also added a Procfile trying to make it work in heroku

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -9,6 +9,6 @@ gulp.task('prod', ['clean'], function(cb) {
 
   global.isProd = true;
 
-  runSequence(['sass', 'imagemin', 'browserify', 'copyFonts', 'copyIndex', 'copyIcons'], cb);
+  runSequence(['sass', 'imagemin', 'browserify', 'copyFonts', 'copyIndex', 'copyIcons'], 'browserSync', cb);
 
 });
